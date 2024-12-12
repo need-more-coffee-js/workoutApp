00e8e73254a682a4686a63715e159ed7b9cbab5f
Dionysus <101218306+need-more-coffee-js@users.noreply.github.com>
2024-12-12 18:53:45 +0300

    Add files via upload

---
+++ workoutApp/workoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  workoutApp
+//
+//  Created by Денис Ефименков on 12.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
