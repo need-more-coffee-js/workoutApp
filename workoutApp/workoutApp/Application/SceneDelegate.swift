6e2dc4382d4cbc5f807b7cf6beacd5405388be16
Denis <bluesky171@mail.ru>
2024-12-12 19:51:00 +0300

    first steps

---
+++ workoutApp/workoutApp/Application/SceneDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  SceneDelegate.swift
+//  workoutApp
+//
+//  Created by Денис Ефименков on 12.12.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+
+        let tabBarController = UITabBarController()
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+    }
+
+   
+}
+
